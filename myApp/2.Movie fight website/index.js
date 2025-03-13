d821b6d448b2906a389717941e71ab6d5aabf60e
Baskoro Ramadhan <baskoror725@gmail.com>
2025-03-13 21:01:19 +0700

    fetch movie id and data

--- myApp/2.Movie fight website/index.js
+++ myApp/2.Movie fight website/index.js
@@ -53,6 +53,7 @@ const onInput = async event => {
     option.addEventListener('click', () =>{
       dropdown.classList.remove('is-active');
       input.value = movie.Title;
+      onMovieSelect(movie);
     })
 
     resultsWrapper.appendChild(option);// insert to results dropdown
@@ -67,3 +68,13 @@ document.addEventListener('click', event =>{
     dropdown.classList.remove('is-active');
   }
 });
+
+const onMovieSelect = async movie => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+      apikey: '25f89051',
+      i : movie.imdbID
+      }
+    });
+    console.log(response.data)
+}
