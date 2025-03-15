ad7aaa0f373f72dae1763968834b3ddd03b11a55
Baskoro Ramadhan <baskoror725@gmail.com>
2025-03-15 22:05:19 +0700

    refactor fetch data

--- myApp/2.Movie fight website/index.js
+++ myApp/2.Movie fight website/index.js
@@ -1,27 +1,36 @@
-//fetch data func
+/* //fetch data func
 const fetchData = async (searchTerm) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: '25f89051',
-      s : searchTerm
-    }
-  });
-
-  if (response.data.Error){
-    return [];
-  }
-
-  return response.data.Search;
+  
 };
-
+ */
 createAutocomplete({
   root: document.querySelector('.autocomplete'),
-  renderOption(movie) {
+  renderOption : (movie) => {
     const imgSrc = movie.Poster === "N/A" ? '' : movie.Poster;
     return `
         <img src = "${imgSrc}"/>
         ${movie.Title} (${movie.Year})
       `;
+  },
+  onOptionSelect : (movie) =>{
+    onMovieSelect(movie);
+  },
+  inputValue : (movie) =>{
+    return movie.Title
+  },
+  fetchData : async (searchTerm) =>{
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: '25f89051',
+        s : searchTerm
+      }
+    });
+  
+    if (response.data.Error){
+      return [];
+    }
+  
+    return response.data.Search;
   }
 });
 
