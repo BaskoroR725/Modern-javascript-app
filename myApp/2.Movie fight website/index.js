e7d9b1dbfc686c47fb586e246db5ea153c24b65b
Baskoro Ramadhan <baskoror725@gmail.com>
2025-03-16 16:31:32 +0700

    extracting awards value

--- myApp/2.Movie fight website/index.js
+++ myApp/2.Movie fight website/index.js
@@ -77,7 +77,17 @@ const movieTemplate = (movieDetail) => {
   const Metascore = parseInt(movieDetail.Metascore);
   const imdbRating = parseFloat(movieDetail.imdbRating);
   const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
-  
+  const awards = movieDetail.Awards.split(' ').reduce((prev, word) =>{
+    const value = parseInt(word);
+
+    if (isNaN(value)){
+      return prev;
+    } else {
+      return prev + value;
+    }
+  }, 0);
+  console.log(awards)
+
   return `
     <article class='media'>
       <figure class = 'media-left'>
