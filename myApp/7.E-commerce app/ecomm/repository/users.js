0b077df6cce3ae7492d59fdca72c6044510ef074
Baskoro Ramadhan <baskoror725@gmail.com>
2025-03-26 23:32:23 +0700

    create repository database to extend

--- myApp/7.E-commerce app/ecomm/repository/users.js
+++ myApp/7.E-commerce app/ecomm/repository/users.js
@@ -1,30 +1,11 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const util = require('util');
+const Repository = require('./repository');
 
 const scrypt = util.promisify(crypto.scrypt);
 
-class usersRepository{
-  constructor(filename){
-    if(!filename){
-      throw new Error('There is a error happen');
-    }
-    this.filename = filename;
-    try{
-      fs.accessSync(this.filename);
-    } catch (err) {
-      fs.writeFileSync(this.filename, '[]');
-    }
-  }
-
-  async getAll(){
-    return JSON.parse(
-      await fs.promises.readFile(this.filename, { 
-        encoding : 'utf8'
-      })
-    );
-  }
-
+class usersRepository extends Repository{
   async create(attrs){
     attrs.id = this.randomId();
 
@@ -51,58 +32,6 @@ class usersRepository{
 
     return hashed === hashedSuppliedBuf.toString('hex');
   }
-  
-  async writeAll(records){
-    await fs.promises.writeFile(
-      this.filename, JSON.stringify(records, null, 2)
-    )
-  }
-
-  randomId(){
-    return crypto.randomBytes(4).toString('hex');
-  }
-
-  async getOne(id){
-    const records = await this.getAll();
-    return records.find(record => record.id === id);
-  }
-
-  async delete(id){
-    const records = await this.getAll();
-    const filteredRecords = records.filter(record => record.id !== id);
-    await this.writeAll(filteredRecords);
-  }
-
-  async update(id, attrs){
-    const records = await this.getAll();
-    const record = records.find(record => record.id === id);
-
-    if(!record){
-      throw new Error(`Record with id ${id} is not found`)
-    }
-
-    Object.assign(record, attrs);// updating attributes to record
-    await this.writeAll(records);
-  }
-
-  async getOneBy(filters){
-    const records = await this.getAll();
-
-    for(let record of records){
-      let found = true;
-
-      //jika key di array tidak sama dengan filters(input)
-      for (let key in filters){
-        if (record[key] !== filters[key] )
-          found = false;
-      }
-
-      if (found){
-        return record;
-      }
-    }
-  }
-
 }
 
 module.exports = new usersRepository('users.json');
